fb38ce008bb64b0a66b7181878d0d1aa65c80ecb
MilosLbr <brankovicmilos@hotmail.com>
2024-09-24 10:09:13 +0200

    angular and tsserver performance improvements

--- lua/mb/lazy/nvim-lspconfig.lua
+++ lua/mb/lazy/nvim-lspconfig.lua
@@ -185,9 +185,33 @@ return {
         },
         angularls = {
           root_dir = util.find_git_ancestor,
+
+          on_init = function(client)
+            -- Disable formatting
+            client.server_capabilities.document_formatting = false
+            client.server_capabilities.document_range_formatting = false
+
+            -- Disable syntax highlighting
+            client.server_capabilities.semantic_tokens_provider = nil
+
+            -- Disable "Go to References"
+            client.server_capabilities.referencesProvider = false
+
+            client.config.capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+          end,
         },
         tsserver = {
           root_dir = util.find_git_ancestor,
+          on_init = function(client)
+            -- Disable formatting
+            client.server_capabilities.document_formatting = false
+            client.server_capabilities.document_range_formatting = false
+
+            -- Disable syntax highlighting
+            client.server_capabilities.semantic_tokens_provider = nil
+
+            client.config.capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+          end,
         },
         eslint = {
           root_dir = util.find_git_ancestor,
