ac24fed32ad3c4c0de818cb81dd996e70e3cf819
MilosLbr <brankovicmilos@hotmail.com>
2024-11-13 15:33:54 +0100

    rename tsserver after update; fix which-key error after update; added mini.starter and flash

--- lua/mb/lazy/nvim-lspconfig.lua
+++ lua/mb/lazy/nvim-lspconfig.lua
@@ -174,8 +174,8 @@ return {
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
         --
         csharp_ls = {
           root_dir = util.root_pattern '*.sln',
@@ -200,7 +200,7 @@ return {
         --     client.config.capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
         --   end,
         -- },
-        tsserver = {
+        ts_ls = {
           root_dir = util.find_git_ancestor,
           on_init = function(client)
             -- Disable formatting
@@ -268,7 +268,7 @@ return {
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
