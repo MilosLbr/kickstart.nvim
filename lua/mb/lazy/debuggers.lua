0259dbc95f1e3fde6b8d26a0ffe1c12c64f9038e
MilosLbr <brankovicmilos@hotmail.com>
2025-06-05 12:07:57 +0200

    easy-dotnet: remove

--- lua/mb/lazy/debuggers.lua
+++ lua/mb/lazy/debuggers.lua
@@ -61,64 +61,64 @@ return {
     require('dap-python').setup()
 
     -- dotnet
-    local dotnet = require 'easy-dotnet'
-    local debug_dll = nil
-
-    local function rebuild_project(co, path)
-      local spinner = require('easy-dotnet.ui-modules.spinner').new()
-      spinner:start_spinner 'Building'
-      vim.fn.jobstart(string.format('dotnet build %s', path), {
-        on_exit = function(_, return_code)
-          if return_code == 0 then
-            spinner:stop_spinner 'Built successfully'
-          else
-            spinner:stop_spinner('Build failed with exit code ' .. return_code, vim.log.levels.ERROR)
-            error 'Build failed'
-          end
-          coroutine.resume(co)
-        end,
-      })
-      coroutine.yield()
-    end
-
-    local function ensure_dll()
-      if debug_dll ~= nil then
-        return debug_dll
-      end
-      local dll = dotnet.get_debug_dll()
-      debug_dll = dll
-      return dll
-    end
-
-    local netcordbg_dir = vim.fn.stdpath 'data' .. '\\mason\\packages\\netcoredbg\\netcoredbg'
-
-    dap.adapters.coreclr = {
-      type = 'executable',
-      command = netcordbg_dir .. '\\netcoredbg.exe',
-      args = { '--interpreter=vscode' },
-    }
-
-    dap.configurations.cs = {
-      {
-        type = 'coreclr',
-        name = 'launch - netcoredbg',
-        request = 'launch',
-        program = function()
-          local dll = ensure_dll()
-          local co = coroutine.running()
-
-          rebuild_project(co, dll.project_path)
-          return dll.relative_dll_path
-        end,
-        cwd = function()
-          local dll = ensure_dll()
-          return dll.relative_project_path
-        end,
-      },
-    }
-
-    dap.listeners.before['event_terminated']['easy-dotnet'] = function()
-      debug_dll = nil
-    end
+    -- local dotnet = require 'easy-dotnet'
+    -- local debug_dll = nil
+    --
+    -- local function rebuild_project(co, path)
+    --   local spinner = require('easy-dotnet.ui-modules.spinner').new()
+    --   spinner:start_spinner 'Building'
+    --   vim.fn.jobstart(string.format('dotnet build %s', path), {
+    --     on_exit = function(_, return_code)
+    --       if return_code == 0 then
+    --         spinner:stop_spinner 'Built successfully'
+    --       else
+    --         spinner:stop_spinner('Build failed with exit code ' .. return_code, vim.log.levels.ERROR)
+    --         error 'Build failed'
+    --       end
+    --       coroutine.resume(co)
+    --     end,
+    --   })
+    --   coroutine.yield()
+    -- end
+    --
+    -- local function ensure_dll()
+    --   if debug_dll ~= nil then
+    --     return debug_dll
+    --   end
+    --   local dll = dotnet.get_debug_dll()
+    --   debug_dll = dll
+    --   return dll
+    -- end
+    --
+    -- local netcordbg_dir = vim.fn.stdpath 'data' .. '\\mason\\packages\\netcoredbg\\netcoredbg'
+    --
+    -- dap.adapters.coreclr = {
+    --   type = 'executable',
+    --   command = netcordbg_dir .. '\\netcoredbg.exe',
+    --   args = { '--interpreter=vscode' },
+    -- }
+    --
+    -- dap.configurations.cs = {
+    --   {
+    --     type = 'coreclr',
+    --     name = 'launch - netcoredbg',
+    --     request = 'launch',
+    --     program = function()
+    --       local dll = ensure_dll()
+    --       local co = coroutine.running()
+    --
+    --       rebuild_project(co, dll.project_path)
+    --       return dll.relative_dll_path
+    --     end,
+    --     cwd = function()
+    --       local dll = ensure_dll()
+    --       return dll.relative_project_path
+    --     end,
+    --   },
+    -- }
+    --
+    -- dap.listeners.before['event_terminated']['easy-dotnet'] = function()
+    --   debug_dll = nil
+    -- end
   end,
 }
